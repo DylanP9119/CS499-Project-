b5ebb7e5112729f480f5c403adc4c533db9543f2
ZeitgeistsAndMicrocosms <wag0014@uah.edu>
2025-03-23 08:51:11 -0500

    Added Day/Night Spawn Conditions
    Finished up part 2/3 of handling day/night differences. Check in with Jacob to make sure that grid position probabilities are weighted and different from day to night.

--- Assets/Scripts/ShipController.cs
+++ Assets/Scripts/ShipController.cs
@@ -8,9 +8,15 @@ public class ShipController : MonoBehaviour
     public GameObject patrolPrefab;
     public GameObject piratePrefab;
 
+    public bool isNight = false;
+
     public float cargoSpawnChance = 0.50f;
     public float patrolSpawnChance = 0.25f;
     public float pirateSpawnChance = 0.40f;
+
+    public float cargoNightChance = 0.50f;
+    public float patrolNightChance = 0.25f;
+    public float pirateNightChance = 0.40f;
 
     float moveTimer = 0.0f;
     private TimeControl isPaused;
@@ -24,10 +30,10 @@ public class ShipController : MonoBehaviour
     }
     void Update()
     {
-        if(isPaused.ShouldMove())
+        if (isPaused.ShouldMove())
         {
-            moveTimer += Time.deltaTime;    
-            if(moveTimer >= 1)   
+            moveTimer += Time.deltaTime;
+            if (moveTimer >= 1)
             {
                 SpawnShip();
                 moveTimer = 0;
@@ -35,12 +41,12 @@ public class ShipController : MonoBehaviour
         }
     }
 
-    void SpawnShip()
+    void SpawnShip() // This function will need to be updated for weighted probabilities. Check in with Jacob.
     {
         HashSet<Vector2Int> occupiedPositions = new HashSet<Vector2Int>(); // Track used spawn positions
 
-        // Cargo Spawn
-        if (Random.value < cargoSpawnChance)
+        // Cargo Day Spawn
+        if (isNight == false && Random.value < cargoSpawnChance)
         {
             Vector3 spawnPos = GetUniqueSpawnPosition("Cargo", occupiedPositions);
             if (spawnPos != Vector3.zero) // Ensures valid position found
@@ -50,8 +56,8 @@ public class ShipController : MonoBehaviour
             }
         }
 
-        // Patrol Spawn
-        if (Random.value < patrolSpawnChance)
+        // Patrol Day Spawn
+        if (isNight == false && Random.value < patrolSpawnChance)
         {
             Vector3 spawnPos = GetUniqueSpawnPosition("Patrol", occupiedPositions);
             if (spawnPos != Vector3.zero)
@@ -61,8 +67,41 @@ public class ShipController : MonoBehaviour
             }
         }
 
-        // Pirate Spawn
-        if (Random.value < pirateSpawnChance)
+        // Pirate Day Spawn
+        if (isNight == false && Random.value < pirateSpawnChance)
+        {
+            Vector3 spawnPos = GetUniqueSpawnPosition("Pirate", occupiedPositions);
+            if (spawnPos != Vector3.zero)
+            {
+                Instantiate(piratePrefab, spawnPos, GetSpawnRotation("Pirate"));
+                Debug.Log($"Pirate Ship Spawned at ({spawnPos.x}, {spawnPos.y})");
+            }
+        }
+
+        // Cargo Night Spawn
+        if (isNight == true && Random.value < cargoNightChance)
+        {
+            Vector3 spawnPos = GetUniqueSpawnPosition("Cargo", occupiedPositions);
+            if (spawnPos != Vector3.zero) // Ensures valid position found
+            {
+                Instantiate(cargoPrefab, spawnPos, GetSpawnRotation("Cargo"));
+                Debug.Log($"Cargo Ship Spawned at ({spawnPos.x}, {spawnPos.y})");
+            }
+        }
+
+        // Patrol Night Spawn
+        if (isNight == true && Random.value < patrolNightChance)
+        {
+            Vector3 spawnPos = GetUniqueSpawnPosition("Patrol", occupiedPositions);
+            if (spawnPos != Vector3.zero)
+            {
+                Instantiate(patrolPrefab, spawnPos, GetSpawnRotation("Patrol"));
+                Debug.Log($"Patrol Ship Spawned at ({spawnPos.x}, {spawnPos.y})");
+            }
+        }
+
+        // Pirate Night Spawn
+        if (isNight == true && Random.value < pirateNightChance)
         {
             Vector3 spawnPos = GetUniqueSpawnPosition("Pirate", occupiedPositions);
             if (spawnPos != Vector3.zero)
@@ -73,7 +112,8 @@ public class ShipController : MonoBehaviour
         }
     }
 
-    Vector3 GetUniqueSpawnPosition(string shipType, HashSet<Vector2Int> occupiedPositions)
+
+    Vector3 GetUniqueSpawnPosition(string shipType, HashSet<Vector2Int> occupiedPositions) // This function will need to be updated for weighted probabilities. Check in with Jacob.
     {
         float roll = Random.value;
         Vector2Int spawnPos;
