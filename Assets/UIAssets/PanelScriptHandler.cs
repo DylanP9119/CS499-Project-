db3fe1a6cc33aa3e9b345cfbe67a80c6bd941787
jtb0062 <jtb0062@uah.edu>
2025-04-15 23:14:18 -0500

    Load Menu text
    Text can now be passed through when creating load menu stuff

---
+++ Assets/UIAssets/PanelScriptHandler.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class PanelScriptHandler : MonoBehaviour
+{ 
+    public GameObject scrollContent;
+
+    //Deletes panel from view.
+    public void DeleteButton()
+    {
+        Destroy(gameObject);
+    }
+
+    public void BeginButton()
+    {
+        string values = "My Save, 04/15/2025, 10:27, 15, 12, ON, 40, 40, 15, 15, 30, 30";
+        string gridText = "this is just test\ndata\n\ntesting";
+        
+        InstantiateTextData(values, gridText);
+    }
+
+    private void InstantiateTextData(string myText, string gridText) {
+        string[] values = myText.Split(',');
+
+        TMP_Text titleText = gameObject.transform.Find("Save Name Text").GetComponent<TMP_Text>();
+        TMP_Text fileDateText = gameObject.transform.Find("Time Made").GetComponent<TMP_Text>();
+        TMP_Text runtimeText = gameObject.transform.Find("Runtime").GetComponent<TMP_Text>();
+        TMP_Text captureAndShipPercentsText = gameObject.transform.Find("Ship Percents").GetComponent<TMP_Text>();
+        TMP_Text gridPercentsText = scrollContent.GetComponentInChildren<TMP_Text>();
+
+        titleText.text = values[0];
+        fileDateText.text = "Created on:\n" + values[1] + ", " + values[2];
+        runtimeText.text = "Days: " + values[3] + "\nHours: " + values[4];
+        captureAndShipPercentsText.text = "2x2 Pirate Night Capture: " + values[5] + "\nCargo: " + values[6] + "% Day, " + values[7] + "% Night " + 
+                                                                                     "\nPatrol: " + values[8] + "% Day, " + values[9] + "% Night " + 
+                                                                                     "\nPirate: " + values[10] + "% Day, " + values[11] + "% Night ";
+        
+        gridPercentsText.text = gridText;
+    }
+}
--- Assets/UIAssets/UILoadMenuController.cs
+++ Assets/UIAssets/UILoadMenuController.cs
@@ -1,6 +1,8 @@
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+using System.Linq;
 
 public class UILoadMenuController : MonoBehaviour
 {
@@ -9,13 +11,31 @@ public class UILoadMenuController : MonoBehaviour
     public GameObject itemPrefab;
     public Transform contentParent; 
 
+    public PanelScriptHandler panelScriptHandler;
+
     public void BackButton()
     {
         SceneManager.LoadScene(mainMenu);
     }
 
-    public void AddItem()
+    //Deletes panel from view.
+    public void DeleteButton()
+    {
+        Destroy(itemPrefab);
+    }
+
+    //Begins the simulation with prompted information.
+    public void StartSim()
+    {
+        //hjere
+    }
+
+    public void AddItemButton()
     {
         Instantiate(itemPrefab, contentParent);
+    }
+
+    public void LoadText() {
+
     }
 }
