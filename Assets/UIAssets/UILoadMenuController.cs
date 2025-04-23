109fc53ccfebba6b20e27293b348c106bb2dd3f4
DylanP9119 <dmp2020@gmail.com>
2025-04-23 01:02:58 -0500

    load fix

--- Assets/UIAssets/UILoadMenuController.cs
+++ Assets/UIAssets/UILoadMenuController.cs
@@ -22,8 +22,8 @@ public class UILoadMenuController : MonoBehaviour
 
     public void PlayButton()
     {
-        SceneManager.LoadScene(simMenu);
         DataPersistence.Instance.wasEnteredfromLoadScene = true;
+        SceneManager.LoadScene(simMenu);
     }
 
 
